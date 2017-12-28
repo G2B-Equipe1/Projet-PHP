2abf95bac284730af0b4a7d83ff4f085775270fb
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2017-12-28 20:59:52 +0100

    Ajout des "première page"

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include 'util.php';
+    start_page();
+    nav_bar();
+?>
+        <header></header>
+
+        <secion>
+            <article></article>
+            <article></article>
+        </secion>
+
+<?php
+
+?>
+
