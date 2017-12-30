b52ccdd5014b26a3cc0d47ba34a92eebe75c9643
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2017-12-30 17:32:24 +0100

    Prise en main de bootstrap

--- util.php
+++ util.php
@@ -5,24 +5,26 @@ function start_page() {
 <!DOCTYPE>
 <html>
     <head>
+        <title>Virtuo Linguo</title>
+        <meta charset="utf-8">
         <link rel="stylesheet" href="css/bootstrap.css"/>
         <link rel="stylesheet" href="css/style.css"/>
     </head>
 
-    <body>
+    <body class="container-fluid">
 EOT;
     echo $start;
 }
 
 function nav_bar() {
     $nav = <<<EOT
-        <nav>
-            <ul>
-                <li><button type="button" onclick="window.location.href = 'index.php';">Acceuil</button></li>
-                <li><button type="button" onclick="window.location.href = 'about.php';">A propos</button> </li>
-                <li><button type="button" onclick="window.location.href = 'contact.php';">Nous contacter</button></li>
-                <li><button type="button" onclick="window.location.href = 'user_space.php';">Espace perso</button></li>
-            </ul>
+        <nav class="container-fluid">
+            <div class="row">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'index.php';">Acceuil</button></div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'about.php';">A propos</button></div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'contact.php';">Nous contacter</button></div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><button class="btn btn-default" type="button" onclick="window.location.href = 'user_space.php';">Espace perso</button></div>
+            </div>
         </nav>
 EOT;
     echo $nav;
