ac04f42b3aa91ea1cbf58401b8110e6f5a5c2912
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2018-01-03 01:30:36 +0100

    Changement au niveau de la barre de navigation

--- util.php
+++ util.php
@@ -8,7 +8,7 @@ function start_page() {
         <title>Virtuo Linguo</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/bootstrap.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="css/style.css"/>        
     </head>
 
     <body class="container-fluid">
@@ -18,12 +18,16 @@ EOT;
 
 function nav_bar() {
     $nav = <<<EOT
-        <nav class="container-fluid">
+        <nav class="container-fluid nav">
             <div class="row">
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'index.php';">Acceuil</button></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'about.php';">A propos</button></div>
-                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><button class="btn btn-default" type="button" onclick="window.location.href = 'contact.php';">Nous contacter</button></div>
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><button class="btn btn-default" type="button" onclick="window.location.href = 'user_space.php';">Espace perso</button></div>
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 nav_col_1">
+                    <button class="btn btn-default button_nav" type="button" onclick="window.location.href = 'index.php';">Acceuil</button>
+                    <button class="btn btn-default button_nav" type="button" onclick="window.location.href = 'about.php';">A propos</button>
+                    <button class="btn btn-default button_nav" type="button" onclick="window.location.href = 'contact.php';">Nous contacter</button>
+                </div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 nav_col_2">
+                    <button class="btn btn-default button_nav hvr-bubble-left" type="button" onclick="window.location.href = 'user_space.php';">User</button>
+                </div>
             </div>
         </nav>
 EOT;
