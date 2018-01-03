e258a7ae1251865897d9fc4b1330799d31f35c3d
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2018-01-03 01:32:49 +0100

    Ajout de faux texte pour tester la page et son style

--- index.php
+++ index.php
@@ -5,36 +5,42 @@
 ?>
         <header></header>
 
-        <div class="container">
-            <secion class="row">
-                <article class="col-xs-12 col-sm-6 col-md-6col-lg-6">
+        <div class="container-fluid section">
+            <section class="row">
+                <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere feugiat enim eget posuere. Sed tincidunt mauris est, sit amet porttitor arcu dignissim iaculis. Praesent sagittis nibh enim, sed maximus arcu sollicitudin convallis. Praesent condimentum mauris et tellus sollicitudin, in tincidunt diam venenatis. Nam consectetur dolor eget leo malesuada, id porta purus imperdiet. Duis finibus velit dolor, id pharetra ex ornare non. Integer dictum convallis faucibus.
-
-                        Ut suscipit lobortis pulvinar. Mauris imperdiet turpis sed nunc finibus finibus. Etiam feugiat bibendum risus, non mattis quam mattis at. Suspendisse in nunc nec dui semper egestas at in massa. Donec eu purus sit amet leo condimentum vulputate. Phasellus laoreet magna in nibh finibus, ut bibendum quam lacinia. Ut vel tellus efficitur, commodo nulla a, fermentum sapien. In at dui gravida, elementum nulla id, iaculis turpis. Aenean ut nibh aliquam, imperdiet diam at, rutrum ex. Nulla sit amet fringilla lorem. Aenean interdum id velit quis congue.
-
-                        Vestibulum a massa egestas, aliquet sapien quis, blandit diam. Morbi fringilla sapien id sagittis consequat. Sed eget odio risus. Duis sapien diam, tempus a nulla a, aliquet aliquet nibh. In hac habitasse platea dictumst. Nulla dignissim lectus erat. Nulla id iaculis elit, eu cursus magna. Phasellus quis convallis neque, quis posuere elit. Nullam aliquam ante ante, at semper dui mattis in. Curabitur imperdiet nulla ut nisl lacinia, a fringilla felis bibendum. Suspendisse facilisis ultrices gravida.
-
-                        Integer at mi malesuada, feugiat dolor ut, eleifend diam. Fusce elementum justo consectetur erat ullamcorper, sit amet aliquet purus efficitur. In at feugiat libero. Suspendisse id luctus eros, eget dictum urna. Phasellus dictum sagittis lacus, a posuere metus tempor sit amet. Quisque magna elit, eleifend a mauris quis, elementum consequat ligula. Nam sed dui dapibus tellus pulvinar fermentum at at felis. Fusce rhoncus, neque non ultrices vehicula, justo lectus fringilla ligula, sed bibendum lorem sapien at justo. Duis at mattis nibh. Ut ut magna vel lectus semper posuere.
-
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque eget justo sit amet dolor fringilla tempor. Curabitur fermentum odio at vestibulum blandit. Quisque rhoncus venenatis leo, ac dictum nulla placerat sit amet. Fusce non condimentum felis. Pellentesque malesuada malesuada imperdiet. Fusce tempor et ipsum a blandit. Aliquam molestie sed sem eget accumsan. Vivamus faucibus risus eu tempor vulputate. Etiam sit amet nunc vel erat semper suscipit. Proin ornare tellus urna, non mollis est dictum id. Vivamus libero ante, lacinia a tempor id, ornare gravida orci.
                     </p>
                 </article>
 
                 <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere feugiat enim eget posuere. Sed tincidunt mauris est, sit amet porttitor arcu dignissim iaculis. Praesent sagittis nibh enim, sed maximus arcu sollicitudin convallis. Praesent condimentum mauris et tellus sollicitudin, in tincidunt diam venenatis. Nam consectetur dolor eget leo malesuada, id porta purus imperdiet. Duis finibus velit dolor, id pharetra ex ornare non. Integer dictum convallis faucibus.
-
-                        Ut suscipit lobortis pulvinar. Mauris imperdiet turpis sed nunc finibus finibus. Etiam feugiat bibendum risus, non mattis quam mattis at. Suspendisse in nunc nec dui semper egestas at in massa. Donec eu purus sit amet leo condimentum vulputate. Phasellus laoreet magna in nibh finibus, ut bibendum quam lacinia. Ut vel tellus efficitur, commodo nulla a, fermentum sapien. In at dui gravida, elementum nulla id, iaculis turpis. Aenean ut nibh aliquam, imperdiet diam at, rutrum ex. Nulla sit amet fringilla lorem. Aenean interdum id velit quis congue.
-
-                        Vestibulum a massa egestas, aliquet sapien quis, blandit diam. Morbi fringilla sapien id sagittis consequat. Sed eget odio risus. Duis sapien diam, tempus a nulla a, aliquet aliquet nibh. In hac habitasse platea dictumst. Nulla dignissim lectus erat. Nulla id iaculis elit, eu cursus magna. Phasellus quis convallis neque, quis posuere elit. Nullam aliquam ante ante, at semper dui mattis in. Curabitur imperdiet nulla ut nisl lacinia, a fringilla felis bibendum. Suspendisse facilisis ultrices gravida.
-
-                        Integer at mi malesuada, feugiat dolor ut, eleifend diam. Fusce elementum justo consectetur erat ullamcorper, sit amet aliquet purus efficitur. In at feugiat libero. Suspendisse id luctus eros, eget dictum urna. Phasellus dictum sagittis lacus, a posuere metus tempor sit amet. Quisque magna elit, eleifend a mauris quis, elementum consequat ligula. Nam sed dui dapibus tellus pulvinar fermentum at at felis. Fusce rhoncus, neque non ultrices vehicula, justo lectus fringilla ligula, sed bibendum lorem sapien at justo. Duis at mattis nibh. Ut ut magna vel lectus semper posuere.
-
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque eget justo sit amet dolor fringilla tempor. Curabitur fermentum odio at vestibulum blandit. Quisque rhoncus venenatis leo, ac dictum nulla placerat sit amet. Fusce non condimentum felis. Pellentesque malesuada malesuada imperdiet. Fusce tempor et ipsum a blandit. Aliquam molestie sed sem eget accumsan. Vivamus faucibus risus eu tempor vulputate. Etiam sit amet nunc vel erat semper suscipit. Proin ornare tellus urna, non mollis est dictum id. Vivamus libero ante, lacinia a tempor id, ornare gravida orci.
                     </p>
                 </article>
-            </secion>
+            </section>
+        </div>
+
+        <div class="container-fluid section">
+            <section class="row">
+                <article class="col-xs-12 col-sm-4 col-md-64 col-lg-4">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere feugiat enim eget posuere. Sed tincidunt mauris est, sit amet porttitor arcu dignissim iaculis. Praesent sagittis nibh enim, sed maximus arcu sollicitudin convallis. Praesent condimentum mauris et tellus sollicitudin, in tincidunt diam venenatis. Nam consectetur dolor eget leo malesuada, id porta purus imperdiet. Duis finibus velit dolor, id pharetra ex ornare non. Integer dictum convallis faucibus.
+                    </p>
+                </article>
+
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere feugiat enim eget posuere. Sed tincidunt mauris est, sit amet porttitor arcu dignissim iaculis. Praesent sagittis nibh enim, sed maximus arcu sollicitudin convallis. Praesent condimentum mauris et tellus sollicitudin, in tincidunt diam venenatis. Nam consectetur dolor eget leo malesuada, id porta purus imperdiet. Duis finibus velit dolor, id pharetra ex ornare non. Integer dictum convallis faucibus.
+                    </p>
+                </article>
+
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere feugiat enim eget posuere. Sed tincidunt mauris est, sit amet porttitor arcu dignissim iaculis. Praesent sagittis nibh enim, sed maximus arcu sollicitudin convallis. Praesent condimentum mauris et tellus sollicitudin, in tincidunt diam venenatis. Nam consectetur dolor eget leo malesuada, id porta purus imperdiet. Duis finibus velit dolor, id pharetra ex ornare non. Integer dictum convallis faucibus.
+                    </p>
+                </article>
+            </section>
         </div>
 
 <?php
