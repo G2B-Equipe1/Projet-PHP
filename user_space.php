73f9b84f6bbbb3ec7b2fec99f1f7f3cb6da89af7
Antonin Gaboriau <antonin.gaboriau@etu.univ-amu.fr>
2018-01-16 14:56:27 +0100

    Mis en place du gettext sur la dernière version de user_space

--- user_space.php
+++ user_space.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_COOKIE['confirmail']))
     {
-        header('Location: activate.php');
+        header('Location : activate.php');
     }
     include_once 'util.php';
     require 'base.php';
@@ -17,39 +17,39 @@
             <section class="row">
                 <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                     <form action="data-processing.php" method="post">
-                        <label>E-mail : <input type="text" name="id"/></label>
-                        <label>Mot de passe : <input type="password" name="mdp"/></label>
+                        <label><?php echo _('E-mail : ') ?><input type="text" name="id"/></label>
+                        <label><?php echo _('Password : ') ?><input type="password" name="mdp"/></label>
                         <input type="submit" name="action" value="Se connecter"/>
                     </form>
                     </p>
                     <p style="display:<?php echo isset($_SESSION['connexionfailed']) ? '' : 'none';
                     unset($_SESSION['connexionfailed']) ?>;">
-                        Le mail et/ou le mot de passe est incorrect.
+                        <?php echo _('Le mail et/ou le mot de passe est incorrect.') ?>
                     </p>
                 </article>
 
                 <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                     <form action="data-processing.php" method="post">
-                        <label>Pseudo : <input type="text" name="pseudo"/></label>
-                        <label>E-mail : <input type="text" name="mail"/></label>
-                        <label>Mot de passe : <input type="password" name="mdp"/></label>
-                        <label>Confirmer mot de passe : <input type="password" name="confirmationmdp"/></label>
+                        <label><?php echo _('Username : ')?><input type="text" name="pseudo"/></label>
+                        <label><?php echo _('E-mail : ')?><input type="text" name="mail"/></label>
+                        <label><?php echo _('Password : ')?><input type="password" name="mdp"/></label>
+                        <label><?php echo _('Password verification')?><input type="password" name="confirmationmdp"/></label>
                         <input type="submit" name="action" value="S'inscrire"/>
                     </form>
 
                     <p style="display:<?php echo isset($_SESSION['mailpris']) ? '' : 'none';
                     unset($_SESSION['mailpris']) ?>;">
-                        Cet email n'est pas conforme ou est déjà utilisé sur le site.
+                        <?php echo _('Invalid E-mail') ?>
                     </p>
 
                     <p style="display:<?php echo isset($_SESSION['pseudopris']) ? '' : 'none';
                     unset($_SESSION['pseudopris']) ?>;">
-                        Ce pseudo est déjà pris par un autre utilisateur.
+                        <?php echo _('Username already taken') ?>
                     </p>
 
                     <p style="display:<?php echo isset($_SESSION['mauvaismdp']) ? '' : 'none';
                     unset($_SESSION['mauvaismdp']) ?>;">
-                        Le mot de passe et la confirmation ne correspondent pas.
+                        <?php echo _('Password verification fail') ?>
                     </p>
 
                 </article>
@@ -65,60 +65,59 @@
 
             <p style="display:<?php echo isset($_SESSION['inscriptionreussie']) ? '' : 'none';
             unset($_SESSION['inscriptionreussie']) ?>;">
-                Bienvenue sur notre site ! <br>
+                Bienvenue sur notre site ! ') ?><br>
             </p>
 
-            <h1>Votre espace perso :</h1><br>
-            Votre email : <?php echo $_SESSION['mail'] ?><br>
-            Votre pseudo : <?php echo $_SESSION['pseudo'] ?><br>
-            Votre catégorie : <?php echo $_SESSION['categorie'] ?><br>
-            Changer de catégorie : <br>
+            <h1><?php echo _('Personal space : ') ?></h1><br>
+            <?php echo _('Your E-mail : ') . $_SESSION['mail'] ?><br>
+            <?php echo _('Your User-name : ') . $_SESSION['pseudo'] ?><br>
+            <?php echo _('Your grade : ') . $_SESSION['categorie'] ?><br>
+            <?php echo _('Change grade : ') ?><br>
             <form action="data-processing.php" method="post">
-                <input type="submit" name="action" value="Passer en standard">
-                <input type="submit" name="action" value="Passer en premium">
-                <input type="submit" name="action" value="Passer en traducteur">
-                <input type="submit" name="action" value="Passer en administrateur">
+                <input type="submit" name="action" value="<?php echo _('Change to standard') ?>">
+                <input type="submit" name="action" value="<?php echo _('Change to premium') ?>">
+                <input type="submit" name="action" value="<?php echo _('Change to traductor') ?>">
+                <input type="submit" name="action" value="<?php echo _('Change to administrator') ?>">
             </form>
 
-            <button href="#" onclick="toggleDisplay(changemdp);return false;" >Changer de mot de passe</button>
+            <button href="#" onclick="toggleDisplay(changemdp);return false;" ><?php echo _('Change password')?></button>
             <div id="changemdp" style="display:none;">
                 <form action="data-processing.php" method="post">
-                    <label>Mot de passe actuel : <input type="password" name="ancienmdp"/></label>
-                    <label>Nouveau mot de passe : <input type="password" name="nouveaumdp"/></label>
-                    <label>Répéter le nouveau mot de passe : <input type="password" name="confirmationmdp"/></label>
+                    <label><?php echo _('Actual password : ') ?><input type="password" name="ancienmdp"/></label>
+                    <label><?php echo _('New password : ') ?><input type="password" name="nouveaumdp"/></label>
+                    <label><?php echo _('New password verifiaction') ?><input type="password" name="confirmationmdp"/></label>
                     <input type="submit" name="action" value="Changer mot de passe">
                 </form>
             </div>
 
             <p style="display:<?php echo isset($_SESSION['changesuccess']) ? '' : 'none';
                                 unset($_SESSION['changesuccess']) ?>;">
-                Changement de mot de passe réussi
+                <?php echo _('Password change sucess') ?>
             </p>
             <p style="display:<?php echo isset($_SESSION['changefail1']) ? '' : 'none';
             unset($_SESSION['changefail1']) ?>;">
-                L'ancien mot de passe est incorrect.
+                <?php echo _('Wrong old password') ?>
             </p>
             <p style="display:<?php echo isset($_SESSION['changefail2']) ? '' : 'none';
             unset($_SESSION['changefail2']) ?>;">
-                Le nouveau mot de passe et la confirmation ne correspondent pas.
+                <?php echo _('New password confirmation fail') ?>
             </p>
 
             <form action="data-processing.php" method="post" >
-                <input type="submit" name="action" value="Se déconnecter">
+                <input type="submit" name="action" value="<?php echo _('Log out')?>">
             </form>
 
-            <button href="#" onclick="toggleDisplay(supprcompte);return false;" >Supprimer mon compte</button>
+            <button href="#" onclick="toggleDisplay(supprcompte);return false;" ><?php echo _('Delete my account')?></button>
             <div id="supprcompte" style="display:<?php echo isset($_SESSION['wrongmdp']) ? '' : 'none'; ?>;">
                 <form action="data-processing.php" method="post">
-                    <label>Veuillez entrer votre mot de passe actuel : <input type="password" name="mdp"/></label>
+                    <label><?php echo _('New password input : ') ?><input type="password" name="mdp"/></label>
 
                     <p style="display:<?php echo isset($_SESSION['wrongmdp']) ? '' : 'none';
                     unset($_SESSION['wrongmdp']) ?>;">
-                        Le mot de passe est incorrect.
+                        <?php echo _('Wrong password') ?>
                     </p>
 
-                    <br>Attention, cette opération est irréversible,
-                    la suppression du compte et de ses données est définitive.
+                    <br><?php echo _('Account deletion warning') ?>
                     <input type="submit" name="action" value="Confirmer la suppression du compte">
                 </form>
             </div>
