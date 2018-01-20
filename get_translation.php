2bbf62b7a26ac9ac99f3a8a098a3dae418371194
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2018-01-20 19:59:58 +0100

    page pour donner une traduction et/ou en demander une en fonction de la categorie de session ouverte

--- get_translation.php
+++ get_translation.php
@@ -14,12 +14,12 @@ if (!(isset($_SESSION['categorie']))) {
     <p> Pour proposer une définition, inscrivez-vous ou connectez-vous <a class="btn btn-default" href="user_space.php"><span class="glyphicon glyphicon-arrow-right"></span> Espace utilisateur</a> </p>
 
 <?php }
-else if ($_SESSION['categorie'] == 'standar') {
+else if ($_SESSION['categorie'] == 'standard') {
 ?>
-    <p>Donnez une traduction du mot <?=$to_translate?> en <?=$to?></p>
+    <p>Donnez une traduction du mot "<?=$to_translate?>" en <?=$to?></p>
     <form action="get_translation-processing.php" method="post">
         <input type="text" name="traduction"/>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Envoyer"/>
     </form>
 
 <?php }
@@ -30,7 +30,7 @@ else if ($_SESSION['categorie'] == 'prenium') {
     <form action="get_translation-processing.php" method="post">
         <input type="text" name="traduction"/>
         <input type="checkbox" name="ask_trad"/>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Envoyer"/>
     </form>
 
 <?php }
