a693da297d92f100050f904ee4487d9989f7b89c
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2018-01-21 16:32:34 +0100

    Corrections

--- get_translation.php
+++ get_translation.php
@@ -14,7 +14,7 @@ if (!(isset($_SESSION['categorie']))) {
     <p> Pour proposer une définition, inscrivez-vous ou connectez-vous <a class="btn btn-default" href="user_space.php"><span class="glyphicon glyphicon-arrow-right"></span> Espace utilisateur</a> </p>
 
 <?php }
-else if ($_SESSION['categorie'] == 'standard') {
+else if ($_SESSION['categorie'] == 'Standard') {
 ?>
     <p>Donnez une traduction du mot "<?=$to_translate?>" en <?=$to?></p>
     <form action="get_translation-processing.php" method="post">
@@ -24,12 +24,11 @@ else if ($_SESSION['categorie'] == 'standard') {
 
 <?php }
 
-else if ($_SESSION['categorie'] == 'prenium') {
+else if ($_SESSION['categorie'] == 'Premium') {
 ?>
     <p>Donnez une traduction du mot <?=$to_translate?> en <?=$to?></p>
     <form action="get_translation-processing.php" method="post">
         <input type="text" name="traduction"/>
-        <input type="checkbox" name="ask_trad"/>
         <input type="submit" value="Envoyer"/>
     </form>
 
