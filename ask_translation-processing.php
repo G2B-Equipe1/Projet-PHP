8db91e09f007e17cb4f12fcfb003cc6f854129c6
Yhoarau <yaho26@hotmail.fr>
2018-01-23 01:28:37 +0100

    Ajout de fonctionnalités des comptes Premium

--- ask_translation-processing.php
+++ ask_translation-processing.php
@@ -1,5 +1,59 @@
 <?php
 session_start();
+require 'base.php';
 
+if(isset($_POST['action'])){
+    $_SESSION['to_translate'] = $_POST['to_ask'];
+    $query = 'SELECT *
+              FROM translation
+              WHERE word=\''.$_POST['to_ask'] . '\' OR translation=\'' . $_POST['to_ask']  . '\'' ;
+
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+// Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+
+    if (mysqli_num_rows($dbResult) == 0) {
+        $_SESSION['detectlang'] = ' Nous n\'avons pas de traduction de \''.$_POST['to_ask'].'\' en base de données <br> <br>';
+        header('Location: ask_translation.php');
+        exit();
+    }
+    $dbRow = mysqli_fetch_assoc($dbResult);
+
+    $_SESSION['detectlang'] = $dbRow['word'] == $_POST['to_ask'] ? 'english<br>' : $dbRow['lang'] . '<br>';
+    header('Location: ask_translation.php');
+    exit();
+ }
+
+if(!isset($_GET['from']) || !isset($_GET['to']) || !isset($_GET['to_ask'])){
+    header('Location: ask_translation.php');
+    exit();
+}
+
+if(strpos($_GET['to_ask'], "'") !== FALSE)
+    $_GET['to_ask'] = str_replace("'", "\'", $_GET['to_ask']);
+
+if($_GET['to'] == $_GET['from'] ){
+    $_SESSION['samelang'] = 'Ne pas selectionner deux fois la même langue <br> ';
+    header('Location: ask_translation.php');
+    exit();
+}
+
+
+$from = $_GET['from'];
+$to = $_GET['to'];
+$to_translate = $_GET['to_ask'];
+
+$query = 'INSERT INTO translation_request (user_id, word, from_lang, to_lang, state ) 
+                      VALUES (\'' . $_SESSION['id'] . '\', \'' . $_GET['to_ask'] . '\', \''
+    . $_GET['from'] . '\', \'' . $_GET['to'] . '\', \'en cours\' )';
+$dbResult = mysqli_query($dbLink, $query);
+
+header('Location: ask_translation.php');
 
 ?>
--- ask_translation.php
+++ ask_translation.php
@@ -1,15 +1,56 @@
 <?php
+require 'base.php';
 include 'util.php';
+include_once 'langues.php';
 session_start();
-
 start_page();
 nav_bar();
+
+if (!isset($_SESSION['categorie']) ||  $_SESSION['categorie'] != 'Premium' && $_SESSION['categorie'] != 'Admin' && $_SESSION['categorie'] != 'Trad'){
+    header('Location: translation.php');
+    exit();
+}
+
 ?>
 
-        <form action="ask_translation-processing.php" method="post">
-            <label>Voulez vous demander la traduction du mot "<?php echo $_SESSION['to_translate']; ?>" en <?php echo $_SESSION['to']; ?> ?</label></br>
-            <input type="submit" name="action" value="demander"/>
-        </form>
+
+    <form action="ask_translation-processing.php" method="post">
+        <input type="text" name="to_ask" <?php set_word($_SESSION['to_translate']); ?>/>
+        <input type="submit" name="action" value="Détecter langue"/>
+        <?php echo $_SESSION['detectlang']; $_SESSION['detectlang'] = '';  ?>
+    </form>
+
+
+
+    Demander une traduction inexistante :
+    <form action="ask_translation-processing.php" method="get">
+        <select name="from">
+            <?php set_options($_SESSION['from']); ?>
+        </select>
+        <input type="text" name="to_ask" <?php set_word($_SESSION['to_translate']); ?>/>
+        <select name="to">
+            <?php set_options($_SESSION['to']); ?>
+        </select>
+        <input type="submit" name="action" value="ask"/>
+    </form>
+
+<?php echo $_SESSION['samelang']; $_SESSION['samelang'] = '' ?>
+
+    Historique de mes demandes : <br>
+<?php
+
+$query = 'SELECT *
+          FROM translation_request
+          WHERE user_id=\''.$_SESSION['id'].'\'';
+$dbResult = mysqli_query($dbLink, $query);
+while ($dbRow = mysqli_fetch_assoc($dbResult)) {
+    echo 'Demande de traduction du mot ' . $dbRow['word'] .
+        ' de la langue ' . $dbRow['from_lang'] . ' à la langue ' . $dbRow['to_lang'] .
+        ', statut : ' . $dbRow['state'] . '<br>';
+}
+
+?>
+
 
 <?php
 footer();
