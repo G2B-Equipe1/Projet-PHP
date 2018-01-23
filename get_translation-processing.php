1182d0cef81a8d1a6da0ee90e4706b4116766781
Yhoarau <yaho26@hotmail.fr>
2018-01-23 01:27:59 +0100

    Ajout de fonctionnalités des comptes traducteur

--- get_translation-processing.php
+++ get_translation-processing.php
@@ -1,4 +1,209 @@
 <?php
+require 'base.php';
+require_once 'langues.php';
 session_start();
+if (!isset($_SESSION['categorie']) || $_SESSION['categorie'] != 'Admin' && $_SESSION['categorie'] != 'Trad') {
+    header('Location: translation.php');
+    exit();
+}
+
+
+
+if(strpos($_POST['to_translate'], "'") !== FALSE)
+    $_POST['to_translate'] = str_replace("'", "\'", $_POST['to_translate']);
+if(strpos($_POST['translation'], "'") !== FALSE)
+    $_POST['translation'] = str_replace("'", "\'", $_POST['translation']);
+
+
+
+
+$action = $_POST['action'];
+
+if($action == 'New translation'){
+    if($_POST['to'] == $_POST['from'] ){
+        $_SESSION['samelang'] = 'Ne pas selectionner deux fois la même langue <br> ';
+        header('Location: get_translation.php');
+        exit();
+    }
+    $today = date('Y-m-d');
+    if($_POST['to'] != 'english' && $_POST['from'] == 'english' ){
+
+        $query = 'INSERT INTO translation (user_id, word, translation, lang, date)
+              VALUES(\'' . $_SESSION['id'] . '\', \'' . $_POST['to_translate'] . '\', \'' .
+            $_POST['translation'] . '\', \'' .$_POST['to'] . '\', \'' . $today . '\' )' ;
+
+        if(!($dbResult = mysqli_query($dbLink, $query)))
+        {
+            echo 'Erreur dans requête<br />';
+            // Affiche le type d'erreur.
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+            // Affiche la requête envoyée.
+            echo 'Requête : ' . $query . '<br/>';
+            exit();
+        }
+        $_SESSION['to_translate'] =  $_POST['to_translate'];
+        $_SESSION['to'] =  $_POST['to'];
+        $_SESSION['add_success'] = 'Traduction ajoutée avec succes <br> <br>';
+        header('Location: get_translation.php');
+    }
+    else if($_POST['to'] == 'english' && $_POST['from'] != 'english') {
+
+        $query = 'INSERT INTO translation (user_id, word, translation, lang, date)
+              VALUES(\'' . $_SESSION['id'] . '\', \'' . $_POST['translation'] . '\', \'' .
+            $_POST['to_translate'] . '\', \'' . $_POST['from'] . '\', \'' . $today . '\' )' ;
+
+        if(!($dbResult = mysqli_query($dbLink, $query)))
+        {
+            echo 'Erreur dans requête<br />';
+            // Affiche le type d'erreur.
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+            // Affiche la requête envoyée.
+            echo 'Requête : ' . $query . '<br/>';
+            exit();
+        }
+        $_SESSION['to_translate'] =  $_POST['to_translate'];
+        $_SESSION['to'] =  $_POST['to'];
+        $_SESSION['add_success'] = 'Traduction ajoutée avec succes <br> <br>';
+        header('Location: get_translation.php');
+    }
+    else{
+        header('Location: get_translation.php');
+        exit();
+    }
+
+}
+else if($action == 'Résoudre'){
+    check_requests();
+    unset($_SESSION['resolve_trad2']);
+    $restrad = 'Résoudre la demande de traduction suivante : <b>' . $_POST['word'] .
+        '</b> de la langue <b>' . $_POST['from_lang'] . '</b> à la langue <b>' . $_POST['to_lang'] . '</b>';
+    if($_POST['from_lang'] == 'english' || $_POST['to_lang'] == 'english' ){
+        $_SESSION['resolve_trad'] = $restrad;
+        $_SESSION['from'] = $_POST['from_lang'];
+        $_SESSION['to'] = $_POST['to_lang'];
+    }
+    else{
+        $restrad .= '<br>Pour cela, remplissez les deux traductions suivantes : <br>';
+        $_SESSION['resolve_trad2'] = $restrad;
+        $_SESSION['tradtemp'] = '';
+        $_SESSION['from'] = $_POST['from_lang'];
+        $_SESSION['from2'] = 'english';
+        $_SESSION['to'] = 'english';
+        $_SESSION['to2'] = $_POST['to_lang'];
+    }
+    $_SESSION['to_translate'] = $_POST['word'];
+    header('Location: get_translation.php');
+    exit();
+}
+else if($action == 'Première étape'){
+    if($_POST['to'] == $_POST['from'] ){
+        $_SESSION['samelang'] = 'Ne pas selectionner deux fois la même langue <br> ';
+        header('Location: get_translation.php');
+        exit();
+    }
+    $_SESSION['tradtemp'] = $_POST['translation'];
+    header('Location: get_translation.php');
+    exit();
+}
+else if($action == 'Seconde étape'){
+    if($_POST['to'] == $_POST['from'] ){
+        $_SESSION['samelang'] = 'Ne pas selectionner deux fois la même langue <br> ';
+        header('Location: get_translation.php');
+        exit();
+    }
+    if($_SESSION['tradtemp'] == ''){
+        $_SESSION['firstfirst'] = '<b>Vous devez d\'abord valider la première étape</b><br>';
+        header('Location: get_translation.php');
+        exit();
+    }
+    $today = date('Y-m-d');
+    $query = 'INSERT INTO translation (user_id, word, translation, lang, date)
+              VALUES(\'' . $_SESSION['id'] . '\', \'' . $_SESSION['tradtemp'] . '\', \'' .
+        $_SESSION['to_translate'] . '\', \'' .$_SESSION['from'] . '\', \'' . $today . '\' )' ;
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    $query = 'INSERT INTO translation (user_id, word, translation, lang, date)
+              VALUES(\'' . $_SESSION['id'] . '\', \'' . $_SESSION['tradtemp'] . '\', \'' .
+        $_POST['translation'] . '\', \'' .$_SESSION['to2'] . '\', \'' . $today . '\' )' ;
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    $_SESSION['add_success'] = 'Traduction ajoutée avec succes <br> <br>';
+    unset($_SESSION['resolve_trad2']);
+    header('Location: get_translation.php');
+}
+else if($action == 'Refuser'){
+
+    $query = 'UPDATE translation_request SET state = \'refusé\' WHERE id = \'' . $_POST['id'] . '\'';
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    header('Location: get_translation.php');
+    exit();
+
+}
+else if($action == 'Modifier'){
+
+    unset($_SESSION['resolve_trad2']);
+    $_SESSION['modiftrad'] = 'Modifier la traduction existante suivante : <b>' . $_POST['word'] .
+        '</b> en <b>' . $_POST['lang'] . '</b> donne <b>' . $_POST['translation'] . '</b>';
+    $_SESSION['modifid'] = $_POST['id'];
+    $_SESSION['from'] = 'english';
+    $_SESSION['to'] = $_POST['lang'];
+    $_SESSION['to_translate'] = $_POST['word'];
+    header('Location: get_translation.php');
+    exit();
+}
+else if($action == 'Modifier traduction'){
+    $query = 'UPDATE translation SET translation = \'' . $_POST['translation'] .
+        '\' WHERE trad_id = \'' . $_SESSION['modifid'] . '\'';
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    header('Location: get_translation.php');
+    exit();
+
+}
+else if($action == 'Supprimer'){
+    $query = 'DELETE FROM translation WHERE trad_id = \'' . $_POST['id'] . '\'';
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    header('Location: get_translation.php');
+    exit();
+}
+
+
 
 ?>
--- get_translation.php
+++ get_translation.php
@@ -1,38 +1,88 @@
 <?php
+include 'base.php';
 include 'util.php';
+include_once 'langues.php';
 session_start();
 start_page();
 nav_bar();
 
-$from = $_SESSION['from'] ;
-$to = $_SESSION['to'];
-$to_translate = $_SESSION['to_translate'];
 
-if (!(isset($_SESSION['categorie']))) {
-?>
+if (!isset($_SESSION['categorie']) || $_SESSION['categorie'] != 'Admin' && $_SESSION['categorie'] != 'Trad') {
+    header('Location: translation.php');
+    exit();
+}
+if(!isset($_SESSION['resolve_trad']) && !isset($_SESSION['resolve_trad2']) && !isset($_SESSION['modiftrad'])){
+    $_SESSION['simpletrad'] = true;
+    echo 'Entrer une nouvelle traduction dans la base de données : ';
+    form_insert_word($_SESSION['to_translate'],'english', 'french');
 
-    <p> Pour proposer une définition, inscrivez-vous ou connectez-vous <a class="btn btn-default" href="user_space.php"><span class="glyphicon glyphicon-arrow-right"></span> Espace utilisateur</a> </p>
+}
+else if(isset($_SESSION['resolve_trad'])){
+    $_SESSION['simpletrad'] = true;
+    echo $_SESSION['resolve_trad'];
+    form_insert_word($_SESSION['to_translate'],$_SESSION['from'], $_SESSION['to']);
+    unset($_SESSION['resolve_trad']);
+}
+else if(isset($_SESSION['resolve_trad2'])){
+    $_SESSION['simpletrad'] = false;
+    $_SESSION['firsttrad'] = true;
+    echo $_SESSION['resolve_trad2'];
+    if($_SESSION['tradtemp'] == '')
+        form_insert_word($_SESSION['to_translate'],$_SESSION['from'], $_SESSION['to']);
+    $_SESSION['firsttrad'] = false;
+    form_insert_word($_SESSION['tradtemp'],$_SESSION['from2'], $_SESSION['to2']);
+    echo $_SESSION['firstfirst'];
+    $_SESSION['firstfirst'] = '';
+    $_SESSION['simpletrad'] = true;
+}
+else if(isset($_SESSION['modiftrad'])){
+    echo $_SESSION['modiftrad'];
+    form_insert_word($_SESSION['to_translate'],$_SESSION['from'], $_SESSION['to']);
+    unset($_SESSION['modiftrad']);
+}
 
-<?php }
-else if ($_SESSION['categorie'] == 'Standard') {
-?>
-    <p>Donnez une traduction du mot "<?=$to_translate?>" en <?=$to?></p>
-    <form action="get_translation-processing.php" method="post">
-        <input type="text" name="traduction"/>
-        <input type="submit" value="Envoyer"/>
-    </form>
+echo $_SESSION['samelang'];
+$_SESSION['samelang'] = '';
+echo $_SESSION['add_success'];
+$_SESSION['add_success'] = '';
 
-<?php }
+ echo 'Toutes les demandes de traductions en cours : <br>';
+check_requests();
 
-else if ($_SESSION['categorie'] == 'Premium') {
-?>
-    <p>Donnez une traduction du mot <?=$to_translate?> en <?=$to?></p>
-    <form action="get_translation-processing.php" method="post">
-        <input type="text" name="traduction"/>
-        <input type="submit" value="Envoyer"/>
-    </form>
+$query = 'SELECT *
+          FROM translation_request
+          WHERE state=\'en cours\'';
+$dbResult = mysqli_query($dbLink, $query);
+while ($dbRow = mysqli_fetch_assoc($dbResult)) {
+    echo 'Demande de traduction du mot <b> ' . $dbRow['word'] .
+        '</b> de la langue <b>' . $dbRow['from_lang'] . '</b> à la langue <b>' . $dbRow['to_lang'] .
+        '</b>, statut : <b>' . $dbRow['state'] . '</b>
+                        <form style="display: inline"  action="get_translation-processing.php" method="post"> 
+                                <input type="hidden" name="id" value="' . $dbRow['id'] .'">
+                                <input type="hidden" name="word" value="' . $dbRow['word'] .'">
+                                <input type="hidden" name="from_lang" value="' . $dbRow['from_lang'] .'">
+                                <input type="hidden" name="to_lang" value="' . $dbRow['to_lang'] .'">
+                                <input type="submit" name="action" value="Résoudre">
+                                <input type="submit" name="action" value="Refuser">
+                        </form>' . '<br>';
+}
 
-<?php }
+echo '<br>Traductions présentes en base de donnée : <br><br>';
+
+$query = 'SELECT * FROM translation';
+$dbResult = mysqli_query($dbLink, $query);
+while ($dbRow = mysqli_fetch_assoc($dbResult)){
+    echo 'Traduction du mot anglais <b> ' . $dbRow['word'] . '</b> en <b>' . $dbRow['lang'] . '</b> : <b>' .
+        $dbRow['translation'] . '</b>
+                        <form style="display: inline"  action="get_translation-processing.php" method="post"> 
+                                <input type="hidden" name="id" value="' . $dbRow['trad_id'] .'">
+                                <input type="hidden" name="word" value="' . $dbRow['word'] .'">
+                                <input type="hidden" name="translation" value="' . $dbRow['translation'] .'">
+                                <input type="hidden" name="lang" value="' . $dbRow['lang'] .'">
+                                <input type="submit" name="action" value="Modifier">
+                                <input type="submit" name="action" value="Supprimer">
+                        </form>' . '<br>';
+}
 
 footer();
 end_page();
