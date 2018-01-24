c6c7df7ccea23d3ea88bba7028fdad5f75fb6648
Romain COLONNA D'ISTRIA <romain.colonna-distria@etu.univ-amu.fr>
2018-01-24 14:43:40 +0100

    Ajout du processus complet de changement de mot de passe

---
+++ edit_password.php
@@ -0,0 +1,19 @@
+<?php
+include 'util.php';
+start_page();
+nav_bar();
+
+if($_GET['verifPasswrds'] == 'goodPasswrds')
+    echo 'Mot de passe changé !';
+else if($_GET['verifPasswrds'] == 'badPasswrds')
+    echo 'Les deux mots de passes ne sont pas identiques.';
+else if($_GET['verifPasswrds'] == 'timeOut')
+    echo 'Temps dépassé, veuillez renouveler votre demande de réinitialisation.';
+
+?>
+
+<form method="post" action="password_reinitialisation.php?token=<?php echo $_GET['token'];?>">
+    <p>Nouveau mot de passe : <input type="password" name="pass" required></p>
+    <p>retapez le nouveau mot de passe : <input type="password" name="pass2" required></p>
+    <input type="submit" name="action" value="modifier"/>
+</form>
---
+++ password_reinitialisation.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'base.php';
+
+if($_POST['pass'] != $_POST['pass2']) {
+    $verifPasswrds = 'badPasswrds';
+}
+else {
+    $token = $_GET['token'];
+    $queryToken = 'SELECT * FROM tokens WHERE token = \'' . $_GET['token'] . '\'';
+
+    if(!($result = mysqli_query($dbLink, $queryToken)))
+        echo 'La requete token a foiré.';
+    $result = mysqli_fetch_assoc($result);
+
+    if(time() >= $result['expire']) {
+        $queryDropToken = 'DELETE FROM tokens WHERE id = \'' . $result['id'] . '\'';
+        if(!($resultDrop = mysqli_query($dbLink, $queryDropToken)))
+            echo 'La requete de suppression du token a foiré.';
+        $verifPasswrds = 'timeOut';
+    }
+    else {
+        $verifPasswrds = 'goodPasswrds';
+
+        $newPasswrd = md5($_POST['pass']);
+        $idUser = $result['idUser'];
+
+        echo $newPasswrd, $idUser;
+        $queryChangePasswrd = 'UPDATE user SET mdp = \'' . $newPasswrd . '\' WHERE id = \'' . $idUser . '\'';
+
+        if(!($result = mysqli_query($dbLink, $queryChangePasswrd)))
+            echo 'La requete de modification de mot de passe a echoué.';
+
+        $queryDropToken = 'DELETE FROM tokens WHERE id = \'' . $result['id'] . '\'';
+        if(!($resultDrop = mysqli_query($dbLink, $queryDropToken)))
+            echo 'La requete de suppression du token a foiré.';
+    }
+
+}
+
+if($verifPasswrds == 'goodPasswrds')
+    header('location: index.php');
+else
+    header('Location: edit_password.php?token=' . $token . '&verifPasswrds=' . $verifPasswrds);
