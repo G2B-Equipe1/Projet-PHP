5f92d142bf0799a05cce785c82ddb70a9bd958f6
Romain Giuntini <romain.giuntini@etu.univ-amu.fr>
2018-01-28 17:19:23 +0100

    Mise en place du about graphiquement

--- vues/about.php
+++ vues/about.php
@@ -3,68 +3,83 @@
 function about_body () {?>
         <div class="container-fluid section">
             <section class="row">
-                <article class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                     <div class="card">
-                        <img src="../img/woman-writing-on-a-notebook.jpeg" class="img-fluid img-responsive rounded" alt="Responsive image">
+                        <img src="img/half-dog.jpeg" class="img-fluid img-responsive img-rounded" alt="Responsive image">
                         <div class="about-crew-member">
-                            <h2>Antonin Gaboriau</h2>
-                            <p>antonin.gaboriau@hotmail.fr</p>
+                            <h2 class="about-name">Antonin GABORIAU</h2>
+                            <h3 class="about-title">Fondateur</h3>
+                            <p> Le meilleur ami du programmeur </p>
+                            <p><b>antonin.GABORIAU@etu.univ-amu.fr</b></p>
+                        <form action="../contact.php" method="post">
+                            <input type="hidden" name="mail" value="antonin.GABORIAU@etu.univ-amu.fr"/>
+                            <button class="btn btn-primary"  type="submit" name="action" value="Contact">Contact</button>
+                        </form>
+                        </div>
+                    </div>
+                </article>
+
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <div class="card">
+                        <img src="img/adorable-cat.jpeg" class="img-fluid img-responsive img-rounded" alt="Responsive image">
+                        <div class="about-crew-member">
+                            <h2 class="about-name">Yael HOARAU</h2>
+                            <h3 class="about-title">Fondateur</h3>
+                            <p> Discret mais efficace quand il veut</p>
+                            <p><b>yael.hoarau@etu.univ-amu.fr</b></p>
                             <form action="../contact.php" method="post">
-                                <input type="hidden" name="mail" value="antonin.gaboriau@hotmail.fr"/>
-                                <button type="submit" name="action" value="Contact" class="button">Contact</button>
+                                <input type="hidden" name="mail" value="yael.hoarau@etu.univ-amu.fr"/>
+                                <button class="btn btn-primary"  type="submit" name="action" value="Contact">Contact</button>
                             </form>
                         </div>
                     </div>
                 </article>
 
-                <article class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                     <div class="card">
-                        <img src="../img/woman-writing-on-a-notebook.jpeg" class="img-fluid img-responsive rounded" alt="Responsive image">
+                        <img src="img/litle-piaf.jpeg" class="img-fluid img-responsive img-rounded" alt="Responsive image">
                         <div class="about-crew-member">
-                            <h2>Jane Doe</h2>
-                            <p class="title">CEO & Founder</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>example@example.com</p>
-                            <p><button class="button">Contact</button></p>
+                            <h2 class="about-name">Romain GIUNTINI</h2>
+                            <h3 class="about-title">Fondateur</h3>
+                            <p> Un peu trop rapide et élégant </p>
+                            <p><b>romain.giuntini@etu.univ-amu.fr</b></p>
+                            <form action="../contact.php" method="post">
+                                <input type="hidden" name="mail" value="romain.giuntini@etu.univ-amu.fr"/>
+                                <button class="btn btn-primary" type="submit" name="action" value="Contact">Contact</button>
+                            </form>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            <section class="row">
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <div class="card">
+                        <img src="img/little-hedgehog.jpeg" class="img-fluid img-responsive img-rounded" alt="Responsive image">
+                        <div class="about-crew-member">
+                            <h2 class="about-name">Adrien CROS</h2>
+                            <h3 class="about-title">Fondateur</h3>
+                            <p> Lent à la détente mais adorable </p>
+                            <p><b>adrien.cros.1@etu.univ-amu.fr</b></p>
+                            <form action="../contact.php" method="post">
+                                <input type="hidden" name="mail" value="adrien.cros.1@etu.univ-amu.fr"/>
+                                <button class="btn btn-primary" type="submit" name="action" value="Contact">Contact</button>
+                            </form>
                         </div>
                     </div>
                 </article>
 
-                <article class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <article class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                     <div class="card">
-                        <img src="../img/woman-writing-on-a-notebook.jpeg" class="img-fluid img-responsive rounded" alt="Responsive image">
+                        <img src="img/red-fish.jpeg" class="img-fluid img-responsive img-rounded" alt="Responsive image">
                         <div class="about-crew-member">
-                            <h2>Jack Doe</h2>
-                            <p class="title">CEO & Founder</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>example@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <div class="card">
-                        <img src="../img/woman-writing-on-a-notebook.jpeg" class="img-fluid img-responsive rounded" alt="Responsive image">
-                        <div class="about-crew-member">
-                            <h2>Joe Doe</h2>
-                            <p class="about-title">CEO & Founder</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>example@example.com</p>
-                            <p><button class="button">Contact</button></p>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <div class="card">
-                        <img src="../img/woman-writing-on-a-notebook.jpeg" class="img-fluid img-responsive rounded" alt="Responsive image">
-                        <div class="about-crew-member">
-                            <h2>Yan Doe</h2>
-                            <p class="title">CEO & Founder</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>example@example.com</p>
-                            <p><button class="button">Contact</button></p>
+                            <h2 class="about-name">Romain COLONNA-DISTRIA</h2>
+                            <h3 class="about-title">Fondateur</h3>
+                            <p> Toujours là même quand il est pas là </p>
+                            <p><b>romain.COLONNA-DISTRIA@etu.univ-amu.fr</b></p>
+                            <form action="../contact.php" method="post">
+                                <input type="hidden" name="mail" value="romain.COLONNA-DISTRIA@etu.univ-amu.fr"/>
+                                <button class="btn btn-primary" type="submit" name="action" value="Contact">Contact</button>
+                            </form>
                         </div>
                     </div>
                 </article>
